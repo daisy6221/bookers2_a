3ca4fbf5f1d1fca807e446aea954bc85517e79d9
MoeMaeda <160582011+moe-maeda@users.noreply.github.com>
2024-03-30 13:44:08 +0000

    [Finish] 5-9a

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,7 @@ class User < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :chats, dependent: :destroy
   has_many :rooms, through: :user_rooms
+  has_many :read_counts, dependent: :destroy
 
   has_one_attached :profile_image
 
